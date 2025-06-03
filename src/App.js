b26c0c8e039d816eff3fa3120cc0f2898672c656
glorijaolujic <gloria.oluja@gmail.com>
2025-06-03 20:35:42 +0200

    dodatni file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Glorija Olujic</h1>
+      <p>Google Media Buyer</p>
+      <br></br>
+      <img
+        src="https://content.r9cdn.net/rimg/dimg/e2/ab/2bd60d83-city-24035-16485134c42.jpg?crop=true&width=1366&height=768&xhint=2802&yhint=1441"
+        alt="Grad Split"
+      />
     </div>
   );
 }
